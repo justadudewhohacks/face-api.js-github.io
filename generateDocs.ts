99f745e2329ec5081b88ca0f836c69a36065de0c
vincent <muehler.v@gmail.com>
2018-11-25 16:46:13 +0100

    generateDocs

--- generateDocs.ts
+++ generateDocs.ts
@@ -2,23 +2,45 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as typedoc from 'typedoc';
 
-const baseDir = './node_modules/face-api.js'
+const faceapiDir = path.resolve('./node_modules/face-api.js/src')
+const tfjsImageRecognitionBaseDir = path.resolve('./node_modules/tfjs-image-recognition-base/src')
+const tfjsTinyYolov2Dir = path.resolve('./node_modules/tfjs-tiny-yolov2/src')
 
-const excludes = [
+const faceapiExcludes = [
   { dir: 'faceLandmarkNet', exceptions: ['index.ts', 'FaceLandmark68Net.ts', 'FaceLandmark68TinyNet.ts'] },
   { dir: 'faceRecognitionNet', exceptions: ['index.ts', 'FaceRecognitionNet.ts'] },
   { dir: 'mtcnn', exceptions: ['index.ts', 'Mtcnn.ts', 'MtcnnOptions.ts'] },
   { dir: 'ssdMobilenetv1', exceptions: ['index.ts', 'SsdMobilenetv1.ts', 'SsdMobilenetv1Options.ts'] },
   { dir: 'tinyFaceDetector', exceptions: ['index.ts', 'TinyFaceDetector.ts', 'TinyFaceDetectorOptions.ts'] },
-  { dir: 'tinyYolov2', exceptions: ['index.ts', 'TinyYolov2.ts'] }
+  { dir: 'tinyYolov2', exceptions: ['index.ts', 'TinyYolov2.ts'] },
+  { dir: 'globalApi', excludes: ['allFaces.ts'] }
 ]
 
-const exclude = excludes.map(({ dir, exceptions }) => {
-  const files = fs.readdirSync(path.resolve(baseDir, 'src'))
-    .filter(file => !exceptions.some(ex => ex === file))
+const tfjsImageRecognitionBaseExcludes = [
+  { dir: 'common', exceptions: [] },
+  { dir: 'metrics', exceptions: [] },
+  { dir: 'utils', exceptions: [] },
+  { dir: '', excludes: ['weightsLoaderFactory.ts'] }
+]
+const tfjsTinyYolov2Excludes = [
+  { dir: 'common', exceptions: [] },
+  { dir: 'tinyYolov2', exceptions: ['TinyYolov2.ts', 'TinyYolov2Options.ts'] },
+]
 
-  return files.map(file => `**/${dir}/${file}`)
-}).reduce((flat, arr) => flat.concat(arr), [])
+const withBaseDir = (baseDir: string) => (obj: any) => ({ ...obj, baseDir})
+
+let exclude = faceapiExcludes.map(withBaseDir(faceapiDir))
+  .concat(tfjsImageRecognitionBaseExcludes.map(withBaseDir(tfjsImageRecognitionBaseDir)))
+  .concat(tfjsTinyYolov2Excludes.map(withBaseDir(tfjsTinyYolov2Dir)))
+  .map(({ baseDir, dir, exceptions, excludes }) => {
+    const moduleDir = path.resolve(baseDir, dir)
+
+    const files = fs.readdirSync(moduleDir)
+      .filter(file => !excludes || excludes.some(ex => ex === file))
+      .filter(file => !(exceptions || []).some(ex => ex === file))
+
+    return files.map(file => `${moduleDir}/${file}`)
+  }).reduce((flat, arr) => flat.concat(arr), [])
 
 const app = new typedoc.Application({
   mode: 'file',
@@ -34,7 +56,8 @@ const app = new typedoc.Application({
 })
 
 const project = app.convert(app.expandInputFiles([
-  path.join(baseDir, 'src'),
-  //'./node_modules/tfjs-image-recognition-base/src'
+  faceapiDir,
+  tfjsImageRecognitionBaseDir,
+  tfjsTinyYolov2Dir
 ]))
 app.generateDocs(project, 'generated/docs')
